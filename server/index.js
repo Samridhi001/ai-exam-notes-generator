afcd48a22fd614c7e7afd37e143a1848f2a5a6fb
Samridhi Shandilya <samridhishandilya118@gmail.com>
2026-02-20 20:57:13 +0530

    Initial backend setup with Express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.json("Server is running");
+});
+
+app.listen(port,()=>{
+  console.log(`server running on port ${port}`);
+});

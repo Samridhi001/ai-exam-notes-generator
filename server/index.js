fd627c6777a4f91ac8c86381f62e251b64fd952e
Samridhi Shandilya <samridhishandilya118@gmail.com>
2026-02-22 14:28:38 +0530

    Successfully connect MongoDB Atlas to backend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./utils/connectDb.js";
+
+dotenv.config();
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const port = 8000;
+app.use(express.json());
+
+connectDb();
 
 app.get("/", (req, res) => {
-  res.json("Server is running");
+  res.json("Server is running 🚀");
 });
 
-app.listen(port,()=>{
-  console.log(`server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
